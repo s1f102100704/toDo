15b49a4211a8ab734c2ad797c0e69d896090a2e9
s1f102100704 <s1f102100704@iniad.org>
2025-02-19 17:30:10 +0900

    フォーム周りのCSS変更

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ type doList = {
   detail:string;
 }
 const App=()=> {
+  const [focusForm,setFocusForm] =useState(false) 
+
   const [toDoList,setToDoList] = useState<doList[]>([])
   const [toDoTitle,setToDoTitle] = useState("")
   const [toDoDetail,setToDoDetail] = useState("") 
@@ -20,25 +22,35 @@ const App=()=> {
     event.preventDefault()
     setToDoList((prev)=>[...prev,{title:toDoTitle,detail:toDoDetail}])
   }
+
+  const focusText = () =>{
+    setFocusForm(true)
+  }
+  const blurText =() =>{
+    setFocusForm(false)
+  } 
+
   return (
     <div className={styles.main}>
-      <div>toDo list</div>
+      <h1 className={styles.title}>今日のやること</h1>
       {toDoList.map((list,index)=>(
-        
-        <div key={index} className={styles.toDoThing}><input type="checkbox"/>
-          <div>{list.title}</div>
-          <div>{list.detail}</div>
+        <div className={styles.toDoThing}>
+          <div className={styles.checkbox}><input type="checkbox" /></div>
+          
+          <div key={index} >
+            <div>{list.title}</div>
+            <div>{list.detail}</div>
+          </div>
         </div>
-      )
-
-      )}
-      <form className={styles.form} onSubmit={addList}>
-       <div className = {styles.textbox}>
-        <input type = "text" className={styles.titleText} onChange={(e)=>getTitleText(e.target.value)}/>
-        <input type= "text" className={styles.detailText} onChange={(e)=>getDetailText(e.target.value)}/>
+      ))}
+      <form className={`${styles.form} ${focusForm ? styles.focused:''} `} onSubmit={addList} >
+       <div className = {styles.textbox}  onFocus={focusText} onBlur={blurText}>
+        <input type = "text" className={styles.titleText} onChange={(e)=>getTitleText(e.target.value)} />
+        <input type= "text" className={styles.detailText} onChange={(e)=>getDetailText(e.target.value)} />
        </div> 
-       
-       <input className = {styles.add} type='submit' value = '追加' />
+       <div className = {styles.submit} >
+        <input className = {styles.add} type='submit' value = '追加' />
+       </div>
       </form>
     </div>
   )
