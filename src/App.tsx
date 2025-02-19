f295cf83520a72f8e3abfd7ec62b7d7b40d10616
s1f102100704 <s1f102100704@iniad.org>
2025-02-19 16:39:07 +0900

    detailの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
 import {  FormEvent, useState } from 'react'
-import './App.css'
-import styles from './index.module.css'
 
+import styles from './index.module.css'
+type doList = {
+  title:string;
+  detail:string;
+}
 const App=()=> {
-  const [toDoList,setToDoList] = useState<string[]>([])
-  const [toDoThing,setToDoThing] = useState("")
-  
-  const getValue = async(value:string)=>{
-    
-    setToDoThing(value);
+  const [toDoList,setToDoList] = useState<doList[]>([])
+  const [toDoTitle,setToDoTitle] = useState("")
+  const [toDoDetail,setToDoDetail] = useState("") 
+
+  const getTitleText = async(value:string)=>{
+    setToDoTitle(value);
+  }
+  const getDetailText =async(value:string)=>{
+    setToDoDetail(value);
   }
   const addList = async(event:FormEvent)=>{
     event.preventDefault()
-    setToDoList((prev)=>[...prev,toDoThing])
-    console.log(toDoList)
+    setToDoList((prev)=>[...prev,{title:toDoTitle,detail:toDoDetail}])
   }
   return (
-    <>
+    <div className={styles.main}>
       <div>toDo list</div>
       {toDoList.map((list,index)=>(
-        <div key={index}>{list}</div>
+        
+        <div key={index} className={styles.toDoThing}><input type="checkbox"/>
+          <div>{list.title}</div>
+          <div>{list.detail}</div>
+        </div>
       )
 
       )}
       <form className={styles.form} onSubmit={addList}>
-       <input type = "text" onChange={(e)=>getValue(e.target.value)}/>
+       <div className = {styles.textbox}>
+        <input type = "text" className={styles.titleText} onChange={(e)=>getTitleText(e.target.value)}/>
+        <input type= "text" className={styles.detailText} onChange={(e)=>getDetailText(e.target.value)}/>
+       </div> 
+       
        <input className = {styles.add} type='submit' value = '追加' />
       </form>
-    </>
+    </div>
   )
 }
 
